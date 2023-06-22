fc179bd44e80d2126437f8633850969c80c50f19
Neha <nehatualea@gmail.com>
2023-06-23 00:34:29 +0530

    Added:Restaurant card

---
+++ src/components/pages/Registration/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextField } from '@mui/material'
+
+const Register = () => {
+  return (
+    <div>
+      <div>Register</div>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+    </div>
+  )
+}
+
+export default Register
