344c0556136c5b867bb453623143e6ee9208afa0
Neha <nehatualea@gmail.com>
2023-06-23 00:36:32 +0530

    data:Added mock data

---
+++ src/utils/constants.js
@@ -0,0 +1,45 @@
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQQ9W98Mg2_uVUCWTJY_WkT0adfjOEXCvHRQ&usqp=CAU";
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const foodQuotes = [
+  "Life is butter with a side of corny jokes.",
+  "Lettuce romaine calm and make food puns.",
+  "I'm on a roll... it's a sushi roll.",
+  "I'm grape-ful for delicious food and cheesy puns.",
+  "Lettuce turnip the beet and have a food-tastic day.",
+  "You're a-peeling! Keep being a-maize-ing!",
+  "Lettuce all taco 'bout how amazing food is.",
+  "You're nacho average food lover, you're extraordinary!",
+  "I'm soy into food puns, they make me smile!",
+  "Donut worry, be happy and enjoy some tasty treats.",
+  "You're egg-ceptional! Keep cooking up greatness.",
+  "You're the zest! Keep adding flavor to the world.",
+  "Life is souper with a side of cheesy puns.",
+  "You're one smart cookie! Keep baking up success.",
+  "You're a tea-riffic person! Keep steeping in happiness.",
+];
+
+export const foodQuotesUrl = [
+  "https://i.pinimg.com/564x/29/2f/9f/292f9faa794226db905bf54365474008.jpg",
+  "https://i.pinimg.com/564x/09/b2/98/09b298c01aeafba3817e7bcbe6db10d5.jpg",
+  "https://i.pinimg.com/564x/99/ec/f4/99ecf4d2e0b363e775989a695650d819.jpg",
+  "https://i.pinimg.com/236x/ea/b6/7b/eab67b15015baf7346c5b09a7d73ea6e.jpg",
+  "https://i.pinimg.com/236x/f7/36/81/f73681ba674ccd74e0c7036b19bff1b1.jpg",
+  "https://i.pinimg.com/236x/5e/c4/ee/5ec4eeb16ed38e70911b09aedcfbdc4b.jpg",
+  "https://i.pinimg.com/236x/b5/f1/cb/b5f1cb04c6ff3342d5e0c538aa00557c.jpg",
+  "https://i.pinimg.com/236x/06/79/41/067941222bf2cdf742ab2714c23c4d21.jpg",
+  "https://i.pinimg.com/564x/71/6d/0a/716d0aa4588d129e2943c16cc39089d2.jpg",
+  "https://i.pinimg.com/736x/ad/8c/eb/ad8ceb0728005867291a81d83bf581c5.jpg",
+  "https://i.pinimg.com/564x/10/d8/d9/10d8d9c8fdfa665a416b41a86f7ba0e0.jpg",
+  "https://i.pinimg.com/236x/c0/d9/b6/c0d9b639750f8b9730d3011c7ac4b796.jpg",
+  "https://i.pinimg.com/236x/3d/41/d5/3d41d559dcdcb076606dd94d2e47d407.jpg",
+  "https://i.pinimg.com/236x/e9/a2/41/e9a2419ab18846b1e158af430f8e5afe.jpg",
+  "https://i.pinimg.com/564x/9c/2a/74/9c2a744057cd42337d3c92ddc61855ae.jpg",
+  "https://i.pinimg.com/236x/71/08/63/7108637a70408bc674b2d8560cbf3cbe.jpg",
+  "https://i.pinimg.com/236x/5b/b6/52/5bb652bd4d302a58fd3c43d9369d736d.jpg",
+  "https://i.pinimg.com/236x/2f/ec/6b/2fec6b552781cf813d5f508daa4f75d9.jpg",
+  "https://i.pinimg.com/236x/5c/88/ff/5c88ff204d7c6316e6b565b7666c3e89.jpg",
+  "https://i.pinimg.com/236x/72/e4/66/72e466d2512cb740b16d564aacc55b0e.jpg",
+  
+];
